556f15a04e13fbf47f64d3dab54873c58b44741f
Enrica <enrica.s92@gmail.com>
2021-10-10 10:48:02 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import yfinance as yf
 
-def main():
 
+def main():
     df = yf.download("AMZN MSFT", start="2019-01-01", end="2020-01-01",
                      group_by="ticker")
     print(df)
     print(df.AMZN)
 
-    Amazon = yf.Ticker("AMZN")
-    print(Amazon.info.keys())
-    print(Amazon.history(period="max"))
+    amazon = yf.Ticker("AMZN")
+    print(amazon.info.keys())
+    print(amazon.history(period="max"))
 
 
 if __name__ == '__main__':
