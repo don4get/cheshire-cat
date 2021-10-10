297d17363697a199941736c98d0d2bed6d0707c8
Anthony De Bortoli <anthony.debortoli@protonmail.com>
2021-10-10 11:09:25 +0200

    Remove dependency to get-all-tickers and fork it manually.

---
+++ cheshire-cat/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""
+__init__.py module containing :class:`~cheshire-cat.__init__.py.<ClassName>` class.
+"""
--- cheshire-cat/__main__.py
+++ cheshire-cat/__main__.py
@@ -1,7 +1,8 @@
 import yfinance as yf
-
+from tickers.get_tickers import *
 
 def main():
+
     df = yf.download("AMZN MSFT", start="2019-01-01", end="2020-01-01",
                      group_by="ticker")
     print(df)
@@ -10,6 +11,10 @@ def main():
     amazon = yf.Ticker("AMZN")
     print(amazon.info.keys())
     print(amazon.history(period="max"))
+
+
+    list_of_tickers = get_tickers()
+    print(list_of_tickers)
 
 
 if __name__ == '__main__':
